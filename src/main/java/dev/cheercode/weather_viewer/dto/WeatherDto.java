ac5b258fe75eb5c0f994f0a818b679bfba900c00
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-09-18 03:43:48 +0700

    Change WeatherDto

--- src/main/java/dev/cheercode/weather_viewer/dto/WeatherDto.java
+++ src/main/java/dev/cheercode/weather_viewer/dto/WeatherDto.java
@@ -1,13 +1,14 @@
 package dev.cheercode.weather_viewer.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalTime;
 
 public record WeatherDto(
         String city, // название города
         String country, // код страны, например "RU", "GB", "CA"
-        Long timezone, // Shift in seconds from UTC. Этот параметр определяет на светлом или на тёмном фоне показываются данные погоды в этой карточке: от восхода до захода солнца фон светлый, до восхода и после захода — тёмный
+        Long timezone,// Shift in seconds from UTC. Этот параметр определяет на светлом или на тёмном фоне показываются данные погоды в этой карточке: от восхода до захода солнца фон светлый, до восхода и после захода — тёмный
 
-        BigDecimal temperature, // Текущая температура. Если выбран язык en, то значение температуры в Fahrenheit, а если ru, то в Celsius
+        BigDecimal temperature,// Текущая температура. Если выбран язык en, то значение температуры в Fahrenheit, а если ru, то в Celsius
         BigDecimal feelsLike, // температура ощущается как
         BigDecimal minTemperature, // минимальная температура дня
         BigDecimal maxTemperature, // максимальная температура дня
@@ -17,7 +18,7 @@ public record WeatherDto(
         // Weather
         String weather, // короткое название погоды (например "ясно")
         String description, // описание погоды (например "переменная облачность")
-        String icon, // номер и буква иконки текущей погоды, например "02d" (сами иконки лежат в директории "static/weather_icon/") имена файлов в таком формате [icon]@2x.png например "02d@2x.png"
+        String icon,// номер и буква иконки текущей погоды, например "02d" (сами иконки лежат в директории "static/weather_icon/") имена файлов в таком формате [icon]@2x.png например "02d@2x.png"
 
         // Wind
         BigDecimal speed, // скорость ветра, если язык ru, то в метрах в секунду, а если en, то в милях в час
@@ -28,7 +29,7 @@ public record WeatherDto(
         BigDecimal rainMillimeters, // Precipitation, mm/h
         BigDecimal snowMillimeters, // Precipitation, mm/h
 
-        Long sunrise, // Sunrise time, unix, UTC
-        Long sunset // Sunset time, unix, UTC
+        LocalTime sunrise, // Sunrise time, unix, UTC
+        LocalTime sunset // Sunset time, unix, UTC
 ) {
 }
